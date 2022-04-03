dc692d72a7f9d401bef6af87d1c338cc3bc75526
Devansh Goel <63224856+Dev-22-goel@users.noreply.github.com>
2022-04-03 11:04:50 -0700

    Modified extra argument error

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -5,15 +5,14 @@ import time
 
 
 class handDetector():
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.5,modelComplexity=1,trackCon=0.5):
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5,trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
-        self.modelComplex = modelComplexity
+#         self.modelComplex = modelComplexity
         self.trackCon = trackCon
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelComplex,
-                                        self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils # it gives small dots onhands total 20 landmark points
 
     def findHands(self, img, draw=True):
